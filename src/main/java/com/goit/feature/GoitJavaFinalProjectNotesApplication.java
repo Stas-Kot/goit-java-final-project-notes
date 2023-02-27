8c0975ed99f23e63e5604ca61e33b7bdd438f50e
Stas Kotelnykov <staskotelnykov@gmail.com>
2023-02-27 21:26:29 +0200

    first commit

---
+++ src/main/java/com/goit/feature/GoitJavaFinalProjectNotesApplication.java
@@ -0,0 +1,13 @@
+package com.goit.feature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoitJavaFinalProjectNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoitJavaFinalProjectNotesApplication.class, args);
+	}
+
+}
